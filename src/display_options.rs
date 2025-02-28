a6e3e3a009d1cf98e8d82b12abaff0293203259c
Ryan Sullivant <rsulli55@gmail.com>
2025-02-27 20:18:31 -0700

    Update the order of fields in DisplayOptions

--- src/display_options.rs
+++ src/display_options.rs
@@ -1,36 +1,39 @@
+/// Struct to hold which counts will be displayed to the user
+/// based on command line options.
 #[derive(Debug, PartialEq)]
 pub struct DisplayOptions {
-    bytes: bool,
-    chars: bool,
     lines: bool,
     words: bool,
+    chars: bool,
+    bytes: bool,
 }
 impl DisplayOptions {
-    pub fn new(bytes: bool, chars: bool, lines: bool, words: bool) -> Self {
+    pub fn new(lines: bool, words: bool, chars: bool, bytes: bool) -> Self {
         Self {
-            bytes,
-            chars,
             lines,
             words,
+            chars,
+            bytes,
         }
     }
 }
 
-// TODO: fix the different order, it has to do with printing like wc
-impl From<&DisplayOptions> for [bool; 4] {
-    fn from(value: &DisplayOptions) -> Self {
-        [value.lines, value.words, value.bytes, value.chars]
+impl Default for DisplayOptions {
+    fn default() -> Self {
+        Self::new(true, true, false, true)
     }
 }
 
+/// Convert `options` to a bool array of length 4.
+impl From<&DisplayOptions> for [bool; 4] {
+    fn from(options: &DisplayOptions) -> Self {
+        [options.lines, options.words, options.chars, options.bytes]
+    }
+}
+
+/// Calculate how many options are turned on.
 pub fn num_to_display(options: &DisplayOptions) -> u8 {
     Into::<[bool; 4]>::into(options)
         .into_iter()
         .fold(0u8, |acc, b| if b { acc + 1 } else { acc })
-}
-
-impl Default for DisplayOptions {
-    fn default() -> Self {
-        Self::new(true, false, true, true)
-    }
 }
